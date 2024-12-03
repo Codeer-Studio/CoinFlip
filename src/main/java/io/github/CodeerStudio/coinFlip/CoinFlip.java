643c97f597d84167755cda1cdb6cc0c00981dead
Thomas Froud <thomasfroud10@gmail.com>
2024-12-03 11:50:46 +0000

    Initial project creation

---
+++ src/main/java/io/github/CodeerStudio/coinFlip/CoinFlip.java
@@ -0,0 +1,17 @@
+package io.github.CodeerStudio.coinFlip;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CoinFlip extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
